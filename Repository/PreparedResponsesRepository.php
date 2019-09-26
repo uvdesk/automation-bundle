c62288918e19afe50ad13ecaae65db740a0a69e4
akshay kumar <akshay.kumar758@webkul.com>
2019-09-26 14:29:32 +0530

    Fix issue #22 - Improve filtering of prepared responses

--- Repository/PreparedResponsesRepository.php
+++ Repository/PreparedResponsesRepository.php
@@ -2,11 +2,9 @@
 
 namespace Webkul\UVDesk\AutomationBundle\Repository;
 
-use Doctrine\ORM\EntityRepository;
-use Doctrine\Common\Collections;
-use Doctrine\ORM\Tools\Pagination\Paginator;
-use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Query;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Collections\Criteria;
 
 /**
  * PreparedResponsesRepository
@@ -16,37 +14,47 @@ use Doctrine\ORM\Query;
  */
 class PreparedResponsesRepository extends EntityRepository
 {
-	public $safeFields = array('page','limit','sort','order','direction');
     const LIMIT = 10;
 
-	public function getPreparesResponses(\Symfony\Component\HttpFoundation\ParameterBag $obj = null, $container) {
+	public $safeFields = ['page','limit','sort','order','direction'];
+
+    public function getPreparesResponses(\Symfony\Component\HttpFoundation\ParameterBag $obj = null, $container)
+    {
         
         $userService = $container->get('user.service');
-       // $currentUser = $userService->getCurrentUser();
-        $json = array();
-        $qb = $this->getEntityManager()->createQueryBuilder();
-        $qb->select('DISTINCT pr.id, pr.name, pr.status, u.id as agentId')->from($this->getEntityName(), 'pr')
+
+        $qb = $this->getEntityManager()->createQueryBuilder()
+            ->select('DISTINCT pr.id, pr.name, pr.status, u.id as agentId')
+            ->from($this->getEntityName(), 'pr')
             ->leftJoin('pr.user', 'ud')
             ->leftJoin('ud.user', 'u');
 
         $data = $obj->all();
         $data = array_reverse($data);
+
         foreach ($data as $key => $value) {
-            if(!in_array($key,$this->safeFields)) {
-                if($key!='dateUpdated' AND $key!='dateAdded' AND $key!='search') {
-                    $qb->Andwhere('pr.'.$key.' = :'.$key);
-                    $qb->setParameter($key, $value);
-                } else {
-                    if($key == 'search') {
-                        $qb->andwhere('pr.name'.' LIKE :name');
-                        $qb->setParameter('name', '%'.urldecode(trim($value)).'%');    
-                    }
-                }
+            switch ($key) {
+                case 'name':
+                case 'description':
+                case 'type':
+                case 'status':
+                    $qb
+                        ->andWhere("pr.$key = :$key")
+                        ->setParameter($key, $value);
+                    break;
+                case 'search':
+                    $qb
+                        ->andWhere('pr.name LIKE :name')
+                        ->setParameter('name', '%' . urldecode(trim($value)) . '%');
+                    break;
+                default:
+                    break;
             }
-        }   
+        }
  
-        if(!isset($data['sort']))
+        if (!isset($data['sort'])) {
             $qb->orderBy('pr.id',Criteria::DESC);
+        }
 
         $paginator  = $container->get('knp_paginator');
 
@@ -71,10 +79,10 @@ class PreparedResponsesRepository extends EntityRepository
             $data[$key]['user'] = $userService->getAgentDetailById($row['agentId']);
         }
 
-        $json['preparedResponses'] = $data;
-        $json['pagination_data'] = $paginationData;
-       
-        return $json;
+        return [
+            'preparedResponses' => $data,
+            'pagination_data' => $paginationData,
+        ];
     }
 
     public function getPreparedResponse($id) 
